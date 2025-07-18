c315d99bd0295d8819462cbf66fee1bf994497b7
gape <gaprielhorn@gmail.com>
2025-07-18 14:04:52 +1000

    feat: build and run script can be run from CLI

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+# Absolute path to this script, e.g. /home/user/bin/foo.sh
+SCRIPT=$(readlink -f "$0") && \
+# Absolute path this script is in, thus /home/user/bin
+SCRIPTPATH=$(dirname "$SCRIPT") && \
+
+cd "$SCRIPTPATH" && \
+# create directory if does not already exist
+mkdir -p build && \
+chmod a+rwx build && \
+g++ -o build/out src/main.cpp `GraphicsMagick++-config --cppflags --cxxflags --ldflags --libs`
+chmod a+rwx build/out
